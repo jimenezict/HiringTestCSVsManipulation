fb827f930b5afeb4e9cd4f3f69a8f837d4b95a7c
jimenezict <jimenezict@gmail.com>
2019-05-23 21:05:23 +0200

    Removing ide files, target and adding .gitignore

---
+++ src/main/java/com/dataontheroad/cctest/service/FileServiceImpl.java
@@ -0,0 +1,28 @@
+package dataontheroad.cctest.service;
+
+import java.io.File;
+import java.util.List;
+
+import datumize.cctest.pojo.InputObject;
+import datumize.cctest.pojo.OutputObject;
+
+public class FileServiceImpl implements FileService{
+
+	@Override
+	public static File getFileByName(String name){
+		
+		return null;
+	}
+
+	@Override
+	public static List<InputObject> readFileByName(File source){
+		
+		return null;
+	}
+
+	@Override
+	public static Integer saveFileByName(String name, List<OutputObject> outputObject){
+		
+		return null;
+	}
+}
